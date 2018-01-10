9d97b8cc3f40596ba48e17cf50beddbdd7d31bc6
Rina KOMAKI <r-komaki@voltage.co.jp>
2018-01-10 12:50:44 +0900

    Hello World

--- src/main/java/codecheck/App.java
+++ src/main/java/codecheck/App.java
@@ -3,7 +3,7 @@ package codecheck;
 public class App {
 	public static void main(String[] args) {
 		for (int i = 0, l = args.length; i < l; i++) {
-			String output = String.format("argv[%s]: %s", i, args[i]);
+			String output = "Hello World!";
 			System.out.println(output);
 		}
 	}
